b3f21065dc3e2ebc8ac3b59d821705f401bd7f90
Sham <roseshammy@gmail.com>
2023-01-06 20:14:22 -0500

    Finished Rock Paper Scissors programming

--- scripts.js
+++ scripts.js
@@ -1,11 +1,74 @@
+const RockButton = document.getElementById("Rock");
+const PaperButton = document.getElementById("Paper");
+const ScissorsButton = document.getElementById("Scissors");
+
+const Options = ["rock", "paper", "scissors"];
+
+// Randomly returns rock paper or scissors
 function getComputerChoice() {
-    // Randomly returns rock paper or scissors
+    return Options[Math.floor(Math.random() * Options.length)];
 }
 
+// Plays a round using the players choice and computers choice
 function playRound(playerSelection, computerSelection) {
-    // Plays a round using the players choice and computers choice
+    if (playerSelection == computerSelection) {
+        game("tie");
+        return "tie";
+    } 
+    if (playerSelection == "rock") {
+        if (computerSelection == "scissors") {
+            game("win");
+            return "You win!";
+        } else {
+            game("lose");
+            return "You lose.";
+        }
+    }
+    if (playerSelection == "paper") {
+        if (computerSelection == "rock") {
+            game("win");
+            return "You win!";
+        } else {
+            game("lose");
+            return "You lose.";
+        }
+    }
+    if (playerSelection == "scissors") {
+        if (computerSelection == "paper") {
+            game("win");
+            return "You win!";
+        } else {
+            game("lose");
+            return "You lose.";
+        }
+    }
 }
 
-function game() {
-    // Plays 5 rounds and determines the winner
+RockButton.addEventListener("click", function() {
+    playRound("rock", getComputerChoice());
+});
+PaperButton.addEventListener("click", function() {
+    playRound("paper", getComputerChoice());
+});
+ScissorsButton.addEventListener("click", function() {
+    playRound("scissors", getComputerChoice());
+});
+
+
+let playerScore = 0
+let computerScore = 0
+
+// Plays 5 rounds and determines the winner
+function game(result) {
+   if (result == "win") {
+        playerScore += 1;
+   } else if (result == "lose") {
+        computerScore += 1;
+   }
+   if (playerScore >= 5) {
+        alert("You win!");
+   } else if (computerScore >= 5) {
+        alert("You lost!");
+   }
 }
+
