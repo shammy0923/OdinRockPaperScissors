8ae5bf3600f95b217512e063eb09ecabedac9b69
Sham <roseshammy@gmail.com>
2023-01-06 19:41:07 -0500

    Setup functions and comments

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function getComputerChoice() {
+    // Randomly returns rock paper or scissors
+}
+
+function playRound(playerSelection, computerSelection) {
+    // Plays a round using the players choice and computers choice
+}
+
+function game() {
+    // Plays 5 rounds and determines the winner
+}
